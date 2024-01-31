4382491f5237ef72a4f3e1466890a8df8141d796
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-01-31 18:49:57 +0300

    min span tree: kruskal's alg

--- mst/mst-kruskal.mjs
+++ mst/mst-kruskal.mjs
@@ -1,5 +1,71 @@
+class DSU {
+  constructor(vertexList=[]) {
+    this.parent = {};
+    this.rank = {};
+
+    for (const v of vertexList) {
+      this.parent[v] = v;
+      this.rank[v] = 0;
+    }
+  }
+
+  find(v) {
+    if (this.parent[v] != v) {
+      this.parent[v] = this.find(this.parent[v]); // Path compression
+    }
+
+    return this.parent[v];
+  }
+
+  union(x, y) {
+    const xRoot = this.find(x);
+    const yRoot = this.find(y);
+
+    if (xRoot == yRoot) {
+      return;
+    }
+
+    // Union by rank
+    if (this.rank[xRoot] < this.rank[yRoot]) {
+      this.parent[xRoot] = yRoot;
+    } else if (this.rank[xRoot] > this.rank[yRoot]) {
+      this.parent[yRoot] = xRoot;
+    } else {
+      this.parent[xRoot] = yRoot;
+      this.rank[yRoot]++;
+    }
+  }
+}
 
 export function mstKruskal(graph) {
+  const edges = [];
 
-  return [];
+  // undirected. edges must not be duplicated
+  for (const fromV in graph.edges) {
+    for (const toV in graph.edges[fromV]) {
+      edges.push({
+        v1: fromV,
+        v2: toV,
+        weight: graph.edges[fromV][toV],
+      });
+    }
+  }
+
+  edges.sort((edge1, edge2) => edge1.weight - edge2.weight);
+
+  const dsu = new DSU(Object.keys(graph.vertexes));
+  const result = [];
+
+  for (const { v1, v2, weight } of edges) {
+    const v1Parent = dsu.find(v1);
+    const v2Parent = dsu.find(v2);
+
+    // Check if adding this edge will form a cycle
+    if (v1Parent != v2Parent) {
+      result.push([v1, v2, weight]);
+      dsu.union(v1Parent, v2Parent);
+    }
+  }
+
+  return result;
 }
--- mst/mst-kruskal.test.mjs
+++ mst/mst-kruskal.test.mjs
@@ -2,34 +2,9 @@ import assert from 'node:assert/strict';
 import test from 'node:test';
 
 import { Graph } from '../create-graph.mjs';
-import { mstPrim } from './mst-prim.mjs';
-import { tsortKahn } from '../tsort/tsort-kahn.mjs';
+import { mstKruskal } from './mst-kruskal.mjs';
 
-const tsortMST = (edges) => {
-  const graphMST = new Graph();
-  
-  edges.forEach(([v1,v2]) => {
-    graphMST.addDirectedEdge(v1,v2);
-  });
-
-  const sortedVertexList = tsortKahn(graphMST);
-
-  const vertexOrderMap = Object.fromEntries(
-    sortedVertexList.map((item,index) => [item, index]),
-  );
-
-  return edges.toSorted((edge1, edge2) => {
-    const firstVertexOrder = vertexOrderMap[edge1[0]] - vertexOrderMap[edge2[0]];
-
-    if (firstVertexOrder === 0) {
-      return vertexOrderMap[edge1[1]] - vertexOrderMap[edge2[1]];
-    }
-
-    return firstVertexOrder;
-  })
-}
-
-test('MST: Prim`s algorithm', () => {
+test('MST: Kruskal`s algorithm', () => {
   // example graph from https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
   const graph = new Graph();
   graph.addEdge('0', '1', 4);
@@ -47,39 +22,25 @@ test('MST: Prim`s algorithm', () => {
   graph.addEdge('6', '8', 6);
   graph.addEdge('7', '8', 7);
 
-  const result = mstPrim(graph);
+  const result = mstKruskal(graph);
 
   assert.deepEqual(
     result,
     [
-      [ '0', '1', 4 ],
-      [ '5', '2', 4 ],
-      [ '2', '3', 7 ],
+      [ '6', '7', 1 ], 
+      [ '2', '8', 2 ], 
+      [ '5', '6', 2 ], 
+      [ '0', '1', 4 ], 
+      [ '2', '5', 4 ], 
+      [ '2', '3', 7 ], 
+      [ '0', '7', 8 ], 
       [ '3', '4', 9 ],
-      [ '6', '5', 2 ],
-      [ '7', '6', 1 ],
-      [ '0', '7', 8 ],
-      [ '2', '8', 2 ],
     ],
   );
 
   assert.equal(
     result.reduce((acc, item) => acc + item[2], 0),
     37,
-  );
-
-  assert.deepEqual(
-    tsortMST(result),
-    [
-      [ '0', '1', 4 ],
-      [ '0', '7', 8 ],
-      [ '7', '6', 1 ],
-      [ '6', '5', 2 ], 
-      [ '5', '2', 4 ],
-      [ '2', '3', 7 ],
-      [ '2', '8', 2 ],
-      [ '3', '4', 9 ],
-    ],
   );
 
   const normalizeEdges = (edges) => edges
